f3abe8579d3e3724adc89820c1529ac793d082ad
Sharath Cherian Thomas <sharathct22@gmail.com>
2020-10-12 18:17:15 +0530

    With progress bars

---
+++ src/resumeData.js
@@ -0,0 +1,132 @@
+import thatapp from "./images/thatapp.jpg";
+import hvac from "./images/hvac.jpg";
+import pickman from "./images/pickman.jpg";
+import plot from "./images/plot.jpg";
+import app from "./images/app.jpg";
+let resumeData = {
+    "imagebaseurl":"https://thesct22.github.io/",
+    "name": "Sharath Cherian Thomas",
+    "role": "Cloud Engineer and Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/sharathct22",
+    "skypeid": "live:cheriansharath5",
+    "roleDescription": "I like to work on multiple stuff and learn even more stuff so I'm always self-sufficient when on a project. Went from IoT to Web Dev to Cloud Enginerring to App Dev and currently DevOps",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sharathct22",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/thesct22",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"whatsapp",
+          "url":"https://wa.me/919048611293",
+          "className":"fab fa-whatsapp"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/TheSCT22",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student at VIT Vellore and pursuing my B.Tech from here. Originally from Electronics and Communications Engineering background, the 'specialization in IoT' part lead me to Web Dev and now DevOps",
+    "address":"Oonnukallel, Chembukadave, Kodenchery, Calicut, Kerala, India.",
+    "website":"https://thesct22.github.io",
+    "education":[
+      {
+        "UniversityName":"Vellore Institute of Technology, Vellore",
+        "specialization":"B. Tech.",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
+        "Achievements":"Ex Technical Head, International Society of Automation- VIT  Section"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Edgate Technologies Pvt. Ltd.",
+        "specialization":"Intern",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked on Texas Instruments Launchpads."
+      },
+      {
+        "CompanyName":"Samsung Prism Project",
+        "specialization":"Intern",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2020",
+        "Achievements":"Worked on Embedded Systems and IoT"
+      }
+    ],
+    "skillsDescription":"Technical Skills",
+    "skills":[
+      {
+        "skillname":"MERN"
+      },
+      {
+        "skillname":"Goole Cloud Computing"
+      },
+      {
+        "skillname":"Docker"
+      },
+      {
+        "skillname":"Andoid Studio"
+      },
+      {
+        "skillname":"Raspberry Pi"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Embedded C"
+      },
+      {
+        "skillname":"C++"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"ThatApp",
+        "description":"Containerized Social Media Website Using MERN Stack",
+        "imgurl":thatapp
+      },
+      {
+        "name":"AC Check",
+        "description":"Monitoring of air-conditioning in warehouses",
+        "imgurl":hvac
+      },
+      {
+        "name":"Pickman",
+        "description":"simple wifi based bot with servo motor arms",  
+        "imgurl":pickman
+      },
+      {
+        "name":"3D Plot",
+        "description":"3D Plotting of cubic interpolated temperature data Bokeh Server on GCP Compute Engine",
+        "imgurl":plot
+      },
+      {
+        "name":"AC App",
+        "description":"Mobile App with Firebase integration and WebView for 3D Plotting",
+        "imgurl":app
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Sharath has been a persistent learner and has enthusiastically applied his skills to develop systems to solve problems. During my tenure at ISA-VIT, I have seen a great development in his technical as well as his interpersonal skills. He is an initiative taker and his position at ISA-VIT is justified.",
+        "name":"Manav Rajvanshi"
+      },
+      {
+        "description":"Amazing friend and a great mentor. I have personally known Sharath from my first year in VIT. Sharath is an energetic and hardworking individual who tries to improve and widen his skillset each day. I could always approach him for any doubts regarding microcontrollers or microprocessors and he was extremely enthusiastic to help me out.",
+        "name":"Christeen T. Jose"
+      }
+    ]
+  }
+  
+  export default resumeData

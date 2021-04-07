380e30c0518a64b09bef449f7de888b909809a7d
thesct22 <sharathct22@gmail.com>
2021-04-07 22:45:59 +0530

    Updates

---
+++ precache-manifest.da19c19e383123057aa9f4eb9e90b7cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e60b5b9b32a44d87c6991140467948f",
+    "url": "/static/media/hvac.4e60b5b9.jpg"
+  },
+  {
+    "revision": "8da0dafa84caaf4006b1",
+    "url": "/static/css/main.d5ddf8bb.chunk.css"
+  },
+  {
+    "revision": "453802f9e42ee52a2a34",
+    "url": "/static/js/1.453802f9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "07cc9e7d3a90decb74fa17236cdb58b2",
+    "url": "/static/media/sharathct.07cc9e7d.pdf"
+  },
+  {
+    "revision": "53307399155614167aa9a5027c5d335a",
+    "url": "/static/media/thatapp.53307399.jpg"
+  },
+  {
+    "revision": "8da0dafa84caaf4006b1",
+    "url": "/static/js/main.8da0dafa.chunk.js"
+  },
+  {
+    "revision": "f2495945e33947777fb8953517888406",
+    "url": "/static/media/pickman.f2495945.jpg"
+  },
+  {
+    "revision": "26443fb80e3ae93e5a8f3b9e65023657",
+    "url": "/static/media/plot.26443fb8.jpg"
+  },
+  {
+    "revision": "833b60a5f0a94d1ae06db3730170baba",
+    "url": "/static/media/app.833b60a5.jpg"
+  },
+  {
+    "revision": "07b0a6d537a624090fb690b4f18e2121",
+    "url": "/static/media/EnvMon.07b0a6d5.jpg"
+  },
+  {
+    "revision": "f4146a5a6eb839eb889193e0ab628e27",
+    "url": "/index.html"
+  }
+];
